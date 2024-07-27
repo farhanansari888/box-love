d51812892d08aa9ff2fed64948bf3b72f35e8f0d
FarhanAnsari <149857451+farhanansari888@users.noreply.github.com>
2024-07-27 14:57:14 +0530

    config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const CONFIG = {
 
     background: "background.jpg",
 
-    min1: "min1.jfif",
-    min2: "min2.jpg",
-    min3: "max3.jpg",
-    min4: "max4.webp",
-    min5: "min5.jpg",
-    min6: "min6.jpg",
+    min1: "IMG-20240710-WA0003.jpg",
+    min2: "IMG-20240710-WA0004.jpg",
+    min3: "IMG-20240710-WA0006.jpg",
+    min4: "IMG-20240710-WA0007.jpg",
+    min5: "IMG-20240710-WA0009.jpg",
+    min6: "IMG-20240710-WA0010.jpg",
 
     max1: "max1.jpg",
     max2: "max2.jpg",
