06052620b82f52bd8e6cb0bf83d08e08364f37d3
FarhanAnsari <149857451+farhanansari888@users.noreply.github.com>
2024-07-27 14:59:42 +0530

    config.js

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ const CONFIG = {
     min5: "IMG-20240710-WA0009.jpg",
     min6: "IMG-20240710-WA0010.jpg",
 
-    max1: "max1.jpg",
-    max2: "max2.jpg",
-    max3: "max3.jpg",
-    max4: "max4.webp",
-    max5: "max5.png",
-    max6: "max6.jpg",
+    max1: "IMG-20240710-WA0003.jpg",
+    max2: "IMG-20240710-WA0004.jpg",
+    max3: "IMG-20240710-WA0006.jpg",
+    max4: "IMG-20240710-WA0007.jpg",
+    max5: "IMG-20240710-WA0009.jpg",
+    max6: "IMG-20240710-WA00010.jpg",
 }
