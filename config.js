7f094a60ad60973154a27bb5fe41b6b35a1b1447
FarhanAnsari <149857451+farhanansari888@users.noreply.github.com>
2024-08-02 22:05:54 +0530

    config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "For Farhan",
+    titleWeb: "For Us❤️",
 
     background: "background.jpg",
 
     min1: "IMG-20240710-WA0003.jpg",
-    min2: "IMG-20240710-WA0004.jpg",
+    min2: "falak1.jpg",
     min3: "IMG-20240710-WA0006.jpg",
-    min4: "IMG-20240710-WA0007.jpg",
+    min4: "falak2.jpg",
     min5: "IMG-20240710-WA0009.jpg",
-    min6: "IMG-20240710-WA0010.jpg",
+    min6: "falak3.jpg",
 
     max1: "IMG-20240710-WA0003.jpg",
-    max2: "IMG-20240710-WA0004.jpg",
+    max2: "falak1.jpg",
     max3: "IMG-20240710-WA0006.jpg",
-    max4: "IMG-20240710-WA0007.jpg",
+    max4: "falak2.jpg",
     max5: "IMG-20240710-WA0009.jpg",
-    max6: "IMG-20240710-WA00010.jpg",
+    max6: "falak3.jpg",
 }
